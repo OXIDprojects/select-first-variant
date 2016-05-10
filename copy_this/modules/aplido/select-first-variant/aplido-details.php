3242f15af72a40efa7ea5eb004357fe67e0ab06f
Florian Schmid <f.schmid@aplido.de>
2016-05-10 16:47:34 +0200

    Intial commit

---
+++ copy_this/modules/aplido/select-first-variant/aplido-details.php
@@ -0,0 +1,24 @@
+<?php
+
+class aplido-details extends aplido-details_parent
+{
+    /**
+     * Checks if current article isn't a variant,
+     * loads variants and if list is not empty redirect to first found variant
+     *
+     * @return  string  parent::render()   parent template file name
+     */
+    public function render()
+    {
+        $oProduct = $this->getProduct();
+
+        if (!$oProduct->oxarticles__oxparentid->value) {
+            $oVariantList = $oProduct->getFullVariants();
+            if($oVariantList){
+                oxRegistry::getUtils()->redirect($oVariantList->current()->getLink(), false);
+            }
+        }
+
+        return parent::render();
+    }
+}
---
+++ copy_this/modules/aplido/select-first-variant/metadata.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'            => 'aplido_variantRedirect',
+    'title'         => 'aplido Varient Redirect',
+    'description'   => 'Wenn ein Vaterartikel aufgerufen wird, wird man automatisch zu der ersten verfügbaren Variante weitergeleitet',
+    'version'       => '1.0',
+    'author'        => 'Aplido GmbH',
+    'url'           => 'http://www.aplido.de',
+    'email'         => 'service@aplido.de',
+    'extend'        => array(
+        'details'   => 'aplido/select-first-variant/aplido-details'
+    )
+);
