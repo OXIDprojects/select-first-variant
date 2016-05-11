9933c8bebcd8464c4e687a6ff0a0389fec7468f5
Florian Schmid <f.schmid@aplido.de>
2016-05-11 09:54:42 +0200

    Changed file- and classnames to match convention

--- copy_this/modules/aplido/select-first-variant/apl_sfv_details.php
+++ copy_this/modules/aplido/select-first-variant/apl_sfv_details.php
@@ -1,6 +1,6 @@
 <?php
 
-class aplido_details extends aplido_details_parent
+class apl_sfv_details extends apl_sfv_details_parent
 {
     /**
      * Checks if current article isn't a variant,
@@ -10,8 +10,9 @@ class aplido_details extends aplido_details_parent
      */
     public function render()
     {
+		
         $oProduct = $this->getProduct();
-
+		
         if (!$oProduct->oxarticles__oxparentid->value) {
             $oVariantList = $oProduct->getFullVariants();
             if($oVariantList){
--- copy_this/modules/aplido/select-first-variant/metadata.php
+++ copy_this/modules/aplido/select-first-variant/metadata.php
@@ -9,14 +9,15 @@ $sMetadataVersion = '1.0';
  * Module information
  */
 $aModule = array(
-    'id'            => 'aplido_variantRedirect',
-    'title'         => 'aplido Varient Redirect',
-    'description'   => 'Wenn ein Vaterartikel aufgerufen wird, wird man automatisch zu der ersten verfügbaren Variante weitergeleitet',
+    'id'            => 'apl_sfv_details',
+    'title'         => '<strong style="color: #0b6eb5;border: 1px solid #F7961E;padding: 0 2px;background:white;">Aplido</strong> Select First Variant',
+    'description'   => 'Wenn ein Vaterartikel aufgerufen wird, wird man automatisch zu der ersten verf&uuml;gbaren Variante weitergeleitet',
     'version'       => '1.0',
     'author'        => 'Aplido GmbH',
     'url'           => 'http://www.aplido.de',
     'email'         => 'service@aplido.de',
+    'thumbnail'    => 'aplido.png',
     'extend'        => array(
-        'details'   => 'aplido/select-first-variant/aplido_details'
+        'details'   => 'aplido/select-first-variant/apl_sfv_details'
     )
 );
